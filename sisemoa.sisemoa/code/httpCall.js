6f45c27daf9f60bbd01f01871cb4b94d2685ec18
moonwon <gomyk@naver.com>
2019-10-13 19:07:20 +0900

    Initialize code for start project

--- sisemoa.sisemoa/code/httpCall.js
+++ sisemoa.sisemoa/code/httpCall.js
@@ -1,53 +1,30 @@
-module.exports.function = function httpCall () {
-//   var http=require('http')
-//   var console=require('console')
-//   var result=[];
+var console = require('console');
+var http = require('http');
 
-//   let options={
-//       format:'json',
-//       headers:{
-//         'X-Naver-Client-Id:':'hIAsGBmDzarXv5spV0Iw',
-//         'X-Naver-Client-Secret:':'TCWfGz9TyM'
-//       },
-//       query: Title
-//   };
+module.exports.function = function httpCall(title) {
 
-//   result=http.getUrl("https://openapi.naver.com/v1/search/shop.json",options);
-//   console.log(result);
-  
+  var base_url = 'https://openapi.naver.com/v1/search/shop';
+  var NAVER_CLIENT_ID = 'hIAsGBmDzarXv5spV0Iw'
+  var NAVER_CLIENT_SECRET = 'TCWfGz9TyM'
 
-//   return result
-// }
-
-var console = ('console')
-// console.log('실행');
-
-// var http = require('http');
-var request = require('request');
-
-const NAVER_CLIENT_ID     = 'hIAsGBmDzarXv5spV0Iw'
-const NAVER_CLIENT_SECRET = 'TCWfGz9TyM'
-
-const option = {
-  query  :'아이패드'
-}
-
-
-request.get({
-    uri:'https://openapi.naver.com/v1/search/shop',
-    qs :option,
-    headers:{
-      'X-Naver-Client-Id':NAVER_CLIENT_ID,
-      'X-Naver-Client-Secret':NAVER_CLIENT_SECRET
+  var result = [];
+  console.log("title : " +title); // title is undefined always
+  let options = {
+    format: 'json',
+    headers: {
+      'X-Naver-Client-Id': NAVER_CLIENT_ID,
+      'X-Naver-Client-Secret': NAVER_CLIENT_SECRET
+    },
+    query: {
+      query : '아이패드' //Todo : replace this value using the parameter
     }
-  }, function(err, res, body) {
-    let json = JSON.parse(body) //json으로 파싱
-    // console.log(json)
-  })
+  };
+  var response = http.getUrl(base_url,options);
+  // result = http.getUrl(base_url, options);
+   console.log(response.items[0]);
 
   return {
-    title: 'test'
-  };
-
+    Title: response.items[0].title,
+    Lprice: response.items[0].lprice
+  }
 }
-
