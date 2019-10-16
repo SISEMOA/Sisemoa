eaf23f2f16ce5b03b7c7029ac5735fae31cb15bd
김민우 <rlaalsdnek11@naver.com>
2019-10-16 20:20:58 +0900

    kim

---
+++ sisemoa.sisemoa/code/SearchGoods.js
@@ -0,0 +1,32 @@
+var console = require('console');
+var http = require('http');
+
+module.exports.function = function SearchGoods(nname) {
+
+  var base_url = 'https://openapi.naver.com/v1/search/shop';
+  var NAVER_CLIENT_ID = 'hIAsGBmDzarXv5spV0Iw'
+  var NAVER_CLIENT_SECRET = 'TCWfGz9TyM'
+
+  var result = [];
+  console.log("name : " +nname); // title is undefined always
+  let options = {
+    format: 'json',
+    headers: {
+      'X-Naver-Client-Id': NAVER_CLIENT_ID,
+      'X-Naver-Client-Secret': NAVER_CLIENT_SECRET
+    },
+    query: {
+      query : nname //Todo : replace this value using the parameter
+    }
+  };
+  var response = http.getUrl(base_url,options);
+  // result = http.getUrl(base_url, options);
+   console.log(response.items);
+
+  return {
+    name: response.items[0].title,
+    price: response.items[0].lprice,
+    iimage: response.items[0].image,
+    link:response.items[0].link
+  }
+}
